8660f708debf034b08625ab4415c17c3fb35c436
Glemmi <Glemmi@Glemmis-iMac.local>
2023-06-23 17:50:11 +0300

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import CustomButton from "./CustomButton";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    Nav,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+}
